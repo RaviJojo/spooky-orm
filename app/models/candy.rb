dfef043c3ce6f6ad4c7023bd880a04a75cfb1260
jnoconor <joe.oconor@gmail.com>
2013-10-25 18:46:07 -0400

    update candy list

--- app/models/candy.rb
+++ app/models/candy.rb
@@ -1,7 +1,7 @@
 class Candy
   attr_accessor :name, :pieces, :size
 
-  NAMES = ["Jelly Beans", "Snickers", "Milky Way", "Reeses", "Peanut Butter Cups", "Swedish Fish", "Kinder Eggs", "Raw Onion", "Pennies"]
+  NAMES = ["Astros", "Cadbury Egg", "Smarties", "M&M's", "Nougat", "Dragon's beard candy", "Cotton candy", "Taffy", "Botan Rice Candy", "Pretzel", "Ice cream", "Caramel", "Chocolate truffle", "Ganache", "Gummi bears", "Gummi cherries", "Menthol", "Chocolate bar", "Sintra", "Rock candy", "Hanukkah gelt", "Galaxy bar", "Dove (chocolate)", "Bit-O-Honey", "Reese's Pieces", "Almond Joy", "Hershey Bar", "Hershey's Kisses", "Kit Kat", "Maltesers", "Milk Duds", "Milky Way", "Rolo", "Snickers", "Twix", "Whoppers", "Cotton candy", "Gumdrops", "M&M's", "Salt water taffy", "Skittles", "Tootsie Roll", "Gobstopper", "Jawbreakers", "Jolly Rancher", "Life Savers", "PEZ", "Red Vines", "Twizzlers", "Lollipop", "Sour Patch Kids", "Sour Punch Straws", "Warheads", "Jelly Beans", "Reese's Peanut Butter Cups", "Swedish Fish", "Kinder Eggs", "Raw Onion", "Pennies"]
   PIECES = (1..15)
   SIZES = ["Mini", "Fun", "Value", "Normal", "King"]
 
